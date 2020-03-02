3026e5406ecffcc754c9a03a161134b88b415d49
csadetec <informaticact@santoagostinho.com.br>
2020-03-02 12:06:36 -0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,74 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react';
+import api from './service/api'
+import Pusher from 'pusher-js'
 
 function App() {
+  const [msg, setMsg] = useState('')
+  const [allMsg, setAllMsg] = useState([])
+
+  useEffect(() => {
+    async function load() {
+      Pusher.logToConsole = true;
+      const pusher = new Pusher('93551fcbc823c6f1fe0a', {
+        cluster: 'us2',
+        forceTLS: true
+      });
+
+      const channel = await pusher.subscribe('my-channel');
+      channel.bind('my-event', data => {
+        let addMsg = allMsg
+        addMsg.push(data)
+        
+        setAllMsg(addMsg)
+        console.log(allMsg)
+
+      });
+    }
+    load()
+
+  }, [allMsg])
+
+
+
+  async function handleSubit(e) {
+    e.preventDefault()
+
+    if (msg === '') {
+      return
+    }
+
+    const { data } = await api.post('/', { msg })
+    let addMsg = allMsg
+    addMsg.push(data)
+
+    setAllMsg(addMsg)
+    //console.log(allMsg)
+    setMsg('')
+
+  }
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <div className="row mt-5">
+        <div className="col-md-6">
+          <form className="form-inline" onSubmit={handleSubit} >
+            <div className="form-group">
+              <input type="text" className="form-control-lg" placeholder="Digita sua mensagem..." value={msg}
+                onChange={(e) => setMsg(e.target.value)}
+              />
+            </div>
+            <button type="submit" className="btn btn-primary">Enviar</button>
+          </form>
+        </div>
+        <div className="col-md-6">
+          <ul className="list-group">
+            {allMsg.map(r =>
+              <li key={r.id} className="list-group-item">{r.msg}</li>
+            )}
+          </ul>
+        </div>
+      </div>
     </div>
   );
 }
